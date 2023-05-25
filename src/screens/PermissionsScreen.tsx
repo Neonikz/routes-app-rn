40631c70e2569df3eac5ec870e1bfea55c6360be
Nicolas Heizenreder <heizenredernicolas@gmail.com>
2023-05-25 15:51:07 -0300

    basic permissions enabled

--- src/screens/PermissionsScreen.tsx
+++ src/screens/PermissionsScreen.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import { PermissionsContext } from '../context/PermissionsContext';
+
 
 export const PermissionsScreen = () => {
+
+    const { permissions, askLocationPermission } = useContext( PermissionsContext );
+
+
     return (
-        <View><Text>Permissions Screen</Text></View>
+        <View style={ styles.container }>
+            <Text style={ styles.title }>Es necesario el uso del GPS para usar esta aplicación </Text>
+
+            <Button
+                title="Permission"
+                onPress={ askLocationPermission }
+            />
+
+            <Text style={{ color:'#000', marginTop: 20 }}>
+                { JSON.stringify( permissions, null, 5 ) }
+            </Text>
+        </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        color:'#000',
+        width: 250,
+        fontSize: 18,
+        textAlign: 'center',
+        marginBottom: 20,
+    }
+});
